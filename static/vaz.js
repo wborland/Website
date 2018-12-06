3bfd149e4d227b101d4ffc6e0ac75350dcfad50d
Will Borland <wborland@purdue.edu>
2018-12-06 00:46:53 -0500

    S3 file uploading and downloading

--- static/vaz.js
+++ static/vaz.js
@@ -1,12 +1,17 @@
 $(document).ready(function() {
+  if(typeof intern != "undefined"){
     $('#intern').DataTable({
       "lengthMenu": [[-1, 25, 50, 100], ["All", 25, 50, 100]]
     });
+  }
 
+
+  $("#file-upload").change(function(){
+    $("#file-name").text(this.files[0].name);
+  });
 
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
-
     gtag('config', 'UA-119549461-1');
 } );
